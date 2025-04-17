debda650f97ac65eebd8f88590f1edb3862c27fd
vinhdq <vinhdq@dfm-engineering.com>
2025-04-17 13:43:32 +0700

    refactor webserver module

--- Firmware_UniversalRemote/main/webserver.c
+++ Firmware_UniversalRemote/main/webserver.c
@@ -6,25 +6,26 @@
 
 static const char *TAG = "WEBSERVER";
 
+
 static esp_err_t url_decode(char *in, char *out)
 {
-    static const char hex_to_dec_arr[] = {
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-         0, 1, 2, 3, 4, 5, 6, 7,  8, 9,-1,-1,-1,-1,-1,-1,
-        -1,10,11,12,13,14,15,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,10,11,12,13,14,15,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1
+    static const char hexstr_to_dec_arr[] = {
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,-1,-1,-1,-1,-1,-1,
+        -1,10,11,12,13,14,15,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,10,11,12,13,14,15,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
     };
     
     
@@ -35,9 +36,10 @@ static esp_err_t url_decode(char *in, char *out)
     
     char c, *iter = out;
     int8_t v1, v2;
+
     while ((c = *in++) != '\0') {
         if (c == '%') {
-            if ((v1 = hex_to_dec_arr[(unsigned char)*in++]) < 0 || (v2 = hex_to_dec_arr[(unsigned char)*in++]) < 0) {
+            if ((v1 = hexstr_to_dec_arr[(unsigned char)*in++]) < 0 || (v2 = hexstr_to_dec_arr[(unsigned char)*in++]) < 0) {
                 *iter = '\0';
                 return ESP_FAIL;
             }
@@ -53,7 +55,7 @@ static esp_err_t http_resp_favicon(httpd_req_t *req)
 {
     extern const unsigned char favicon_ico_start[] asm("_binary_favicon_ico_start");
     extern const unsigned char favicon_ico_end[]   asm("_binary_favicon_ico_end");
-    const size_t favicon_ico_size = (favicon_ico_end - favicon_ico_start);
+    const size_t favicon_ico_size = favicon_ico_end - favicon_ico_start;
     httpd_resp_set_type(req, "image/x-icon");
     httpd_resp_send(req, (const char *)favicon_ico_start, favicon_ico_size);
     return ESP_OK;
@@ -75,12 +77,10 @@ static esp_err_t http_resp_tv_remote(httpd_req_t *req)
 {   
     char *pch =strrchr(req->uri,'/');
     long num_dev = strtol(pch + 1, NULL, 10);
-    
+    extern const unsigned char tv_remote_html_start[] asm("_binary_tv_remote_html_start");
+    extern const unsigned char tv_remote_html_end[] asm("_binary_tv_remote_html_end");
+    const size_t tv_remote_html_size = tv_remote_html_end - tv_remote_html_start;    
     if (num_dev > 0 && num_dev <= 5) {
-        extern const unsigned char tv_remote_html_start[] asm("_binary_tv_remote_html_start");
-        extern const unsigned char tv_remote_html_end[] asm("_binary_tv_remote_html_end");
-        const size_t tv_remote_html_size = (tv_remote_html_end - tv_remote_html_start);
-
         httpd_resp_send_chunk(req, (const char *)tv_remote_html_start, tv_remote_html_size);
         httpd_resp_sendstr_chunk(req, NULL);
         return ESP_OK;
@@ -96,7 +96,7 @@ static esp_err_t http_resp_tv_remote_command(httpd_req_t *req)
     long num_dev = strtol(pch + 1, NULL, 10) - 1;
     
     long ir_code = 0;
-    char buf[32];
+    static char buf[32];
     int ret, remaining = req->content_len;
     while (remaining > 0) 
     {
@@ -118,6 +118,7 @@ static esp_err_t http_resp_tv_remote_command(httpd_req_t *req)
         ir_add_code_tv_detect(ir_code, num_dev);
     }    
     httpd_resp_send(req, NULL, 0);
+    memset(buf, '\0', sizeof(buf));
     return ESP_OK;
 }
 
@@ -125,12 +126,10 @@ static esp_err_t http_resp_ac_remote(httpd_req_t *req)
 {   
     char *pch =strrchr(req->uri,'/');
     long num_dev = strtol(pch + 1, NULL, 10);
-
+    extern const unsigned char ac_remote_html_start[] asm("_binary_ac_remote_html_start");
+    extern const unsigned char ac_remote_html_end[] asm("_binary_ac_remote_html_end");
+    const size_t ac_remote_html_size = ac_remote_html_end - ac_remote_html_start;
     if (num_dev > 0 && num_dev <= 5) {
-        extern const unsigned char ac_remote_html_start[] asm("_binary_ac_remote_html_start");
-        extern const unsigned char ac_remote_html_end[] asm("_binary_ac_remote_html_end");
-        const size_t ac_remote_html_size = (ac_remote_html_end - ac_remote_html_start);
-
         httpd_resp_send_chunk(req, (const char *)ac_remote_html_start, ac_remote_html_size);
         httpd_resp_sendstr_chunk(req, NULL);
         return ESP_OK;
@@ -145,8 +144,7 @@ static esp_err_t httpd_resp_setwifi(httpd_req_t *req)
     if (req->method == HTTP_GET) {
         extern const unsigned char login_html_start [] asm("_binary_login_html_start");
         extern const unsigned char login_html_end [] asm("_binary_login_html_end");
-        const size_t login_html_size = (login_html_end - login_html_start);
-
+        const size_t login_html_size = login_html_end - login_html_start;
         httpd_resp_send_chunk(req, (const char*)login_html_start, login_html_size);
         httpd_resp_sendstr_chunk(req, NULL);
     } else {
@@ -183,6 +181,8 @@ static esp_err_t httpd_resp_setwifi(httpd_req_t *req)
         ESP_LOGI(TAG, "PWD: %s", form_pwd);
         set_wifi(form_ssid, form_pwd);
         httpd_resp_send(req, NULL, 0);
+        memset(buf_raw, '\0', sizeof(buf_raw));
+        memset(buf_decode, '\0', sizeof(buf_decode));
     }
     return ESP_OK;
 }
